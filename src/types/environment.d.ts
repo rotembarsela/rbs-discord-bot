97710c0b7efe58cefa170fedd6eb4ee339c2151f
rbsPREET <rotem.barsela95@gmail.com>
2023-10-06 15:03:45 +0300

    environment file change

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,10 +3,10 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      ENV: "test" | "dev" | "prod";
       DISCORD_TOKEN: string;
       DISCORD_CLIENT_ID: string;
       DISCORD_GUILD_ID: string;
-      ENV: "test" | "dev" | "prod";
     }
   }
 }
